570f3dec5c9a7a2aef1eea9a9881a5ba78397462
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-02-10 15:29:30 +0500

    make this hero section responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <div className='bg-slate-900 text-white p-4'>
-      This is movie app using react
-    </div>
+    <main className='background h-screen overflow-y-auto w-full text-white text-center px-5 md:px-20 py-10 flex flex-col items-center '>
+      
+      {/* Hero Image */}
+      <img className='cursor-pointer' src="hero.png" alt="errro while rendering hero image" />
+
+      {/* Hero Heading */}
+      <h1 className='w-[80%] md:w-[50%] text-5xl font-bold text-white mb-10 text-center'>
+        Find <span className='text-gradient'>Movies</span> You’ll Love Without the Hassle
+      </h1>
+
+      {/* Search Feature */}
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      <h1>
+        {
+          import.meta.env.VITE_BASE_URL
+        }
+      </h1>
+
+    </main>
   )
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value)
+    }
+
+  return (
+    <div className='w-[100%] md:w-[500px] flex items-center gap-4 p-3 rounded bg-dark-100 text-white'>
+        <img className='cursor-pointer' src="search.svg" alt="error while rendering search icon" />
+        <input type="text" value={searchTerm} onChange={handleChange} className='w-full bg-transparent outline-none' placeholder='Search through 300+ movies online' />
+    </div>
+  )
+}
+
+export default Search

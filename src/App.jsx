248d5599bc4e057fae23c9092c9f96dc39f8e10d
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-02-10 21:54:21 +0500

    added the fetching functionality and search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './components/Search'
+import Spinner from './components/Spinner'
+import { useDebounce } from 'react-use'
+import MovieCard from './components/MovieCard'
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [deboundedTerm, setDebouncedTerm] = useState('')
+  const [movies, setMovies] = useState([])
+  const [errorMessage, setErrorMessage] = useState('Error while fetching movies, please try later.')
+
+  useDebounce(() => setDebouncedTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
+
+    setIsLoading(true)
+
+    try {
+
+      const response = await fetch(query ? `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}` : 'https://api.themoviedb.org/3/discover/movie?page=1&sort_by=popularity.desc', {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+        }
+      })
+
+      const data = await response.json()
+      if (data) {
+        setMovies(data.results)
+      }
+
+    } catch (error) {
+      console.log("error while fetching movies: ", error.message)
+      setErrorMessage('Error while fetching movies, please try later.')
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+
+  useEffect(() => {
+    fetchMovies(deboundedTerm)
+  }, [deboundedTerm])
 
   return (
-    <main className='background h-screen overflow-y-auto w-full text-white text-center px-5 md:px-20 py-10 flex flex-col items-center '>
-      
+    <main className='background h-screen overflow-y-auto w-full text-white px-5 md:px-20 py-10 flex flex-col items-center '>
+
       {/* Hero Image */}
-      <img className='cursor-pointer' src="hero.png" alt="errro while rendering hero image" />
+      <img className='cursor-pointer' src="hero.png" alt="error while rendering hero image" />
 
       {/* Hero Heading */}
       <h1 className='w-[80%] md:w-[50%] text-5xl font-bold text-white mb-10 text-center'>
@@ -19,11 +61,33 @@ const App = () => {
       {/* Search Feature */}
       <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 
-      <h1>
+      {/* Populat Heading */}
+      <div className='text-left w-full mt-2'>
+        <h1 className='text-3xl font-semibold text-white text-left'>
+          Popular
+        </h1>
+      </div>
+
+      {/* Populat Movies */}
+      <div className='my-5 w-full'>
         {
-          import.meta.env.VITE_BASE_URL
+          isLoading ? (
+            <Spinner />
+          ) : (
+            movies.length == 0 ? (
+              <p className='text-red-500'>{errorMessage}</p>
+            ) : (
+              <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+                {
+                  movies.map(movie => (
+                    <MovieCard key={movie.id} movie={movie} />
+                  ))
+                }
+              </div>
+            )
+          )
         }
-      </h1>
+      </div>
 
     </main>
   )
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const MovieCard = ({movie: {title, poster_path, vote_average, release_date, original_language}}) => {
+
+  return (
+    <div className='p-5 shadow-inner shadow-dark-100 rounded-lg cursor-pointer'>
+        <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : 'no-poster.png'} alt="" />
+        <h1 className='text-left my-2 font-semibold'>
+            {
+                title
+            }
+        </h1>
+        <div className='mt-2 flex gap-1 items-center'>
+            <img src="star.svg" alt="error while rendering ster icon" />
+            <p className='font-semibold'>{vote_average.toFixed(1)}</p>
+            <span className='text-gray-100'>•</span>
+            <p className='font-semibold text-gray-100'>{release_date.split("-")[0]}</p>
+            <span className='text-gray-100'>•</span>
+            <p className='font-semibold text-gray-100'>{original_language}</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard

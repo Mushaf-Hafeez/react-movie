12a0dc096d8590e5621c88345f0f7c1d4742069a
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-02-10 10:10:36 +0500

    created a vite react app with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-900 text-white p-4'>
+      This is movie app using react
+    </div>
+  )
+}
+
+export default App
